98a47215b5391bc6a92d37533f56b2f77fc45f34
ytakegawa <ytkgw1986@gmail.com>
2021-10-19 16:57:15 +0900

    Updata 404 error message
    ルーティングに404エラーを追加

--- app.rb
+++ app.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
-require 'sinatra'
-require 'sinatra/reloader'
-require 'json'
-require 'cgi'
+require "sinatra"
+require "sinatra/reloader"
+require "json"
+require "cgi"
 
-DB_PATH = 'json/memo_db.json'
+DB_PATH = "json/memo_db.json"
 DB_DATA =
   File.open(DB_PATH) do |file|
     JSON.parse(file.read, symbolize_names: true)
@@ -26,42 +26,42 @@ class Memo
 
   def self.rewrite_json(memo_data)
     memo_to_hash = { memos: memo_data }
-    File.open(DB_PATH, 'w') { |file| JSON.dump(memo_to_hash, file) }
+    File.open(DB_PATH, "w") { |file| JSON.dump(memo_to_hash, file) }
   end
 
   def self.match_id(id)
-    match_memo = ''
+    match_memo = ""
     DB_MEMOS.each { |memo| match_memo = memo if memo[:id] == id.to_i }
     match_memo
   end
 end
 
-get '/' do
+get "/" do
   @memos = DB_MEMOS
   erb :index
 end
 
-get '/new' do
+get "/new" do
   erb :new
 end
 
-get '/show/:id' do
+get "/show/:id" do
   @memo = Memo.match_id(params[:id])
   erb :show
 end
 
-get '/edit/:id' do
+get "/edit/:id" do
   @memo = Memo.match_id(params[:id])
   erb :edit
 end
 
-post '/new' do
+post "/new" do
   memo = Memo.new(CGI.escape_html(params[:title]), CGI.escape_html(params[:body]))
   Memo.rewrite_json(memo.add_memo)
-  redirect '/'
+  redirect "/"
 end
 
-patch '/edit-done/:id' do
+patch "/edit-done/:id" do
   match_memo = Memo.match_id(params[:id])
   change_memo =
     DB_MEMOS.each do |memo|
@@ -71,16 +71,20 @@ patch '/edit-done/:id' do
       end
     end
   Memo.rewrite_json(change_memo)
-  redirect '/'
+  redirect "/"
   erb :index
 end
 
-delete '/delete/:id' do
+delete "/delete/:id" do
   delete_memo =
     DB_MEMOS.each do |memo|
       DB_MEMOS.delete(memo) if memo[:id] == params[:id].to_i
     end
   Memo.rewrite_json(delete_memo)
-  redirect '/'
+  redirect "/"
   erb :index
+end
+
+error 404 do
+  "404 not found sorry.."
 end
