ccc8d31f94e95e978961464699735622be1fc98b
ytakegawa <ytkgw1986@gmail.com>
2021-10-28 15:12:41 +0900

    Refactoring app.rb
    メンターの指摘事項修正

--- app.rb
+++ app.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
-require "sinatra"
-require "sinatra/reloader"
-require "json"
-require "cgi"
+require 'sinatra'
+require 'sinatra/reloader'
+require 'json'
+require 'cgi'
 
-DB_PATH = "json/memo_db.json"
+DB_PATH = 'json/memo_db.json'
 DB_DATA =
   File.open(DB_PATH) do |file|
     JSON.parse(file.read, symbolize_names: true)
@@ -14,77 +14,92 @@ DB_MEMOS = DB_DATA[:memos]
 
 class Memo
   def initialize(title, body)
-    @id = DB_MEMOS.empty? ? 1 : DB_MEMOS[-1][:id] + 1
-    @title = title
-    @body = body
+    @title = CGI.escape_html(title)
+    @body = CGI.escape_html(body)
   end
 
   def add_memo
-    new_memo = { id: @id, title: @title, body: @body }
-    DB_MEMOS << new_memo
+    id = DB_MEMOS.empty? ? 1 : DB_MEMOS[-1][:id] + 1
+    DB_DATA[:memos] << { id: id, title: @title, body: @body }
   end
 
-  def self.rewrite_json(memo_data)
+  def change_memo(id)
+    DB_MEMOS.each do |memo|
+      if memo[:id] == id.to_i
+        memo[:title] = @title
+        memo[:body] = @body
+      end
+    end
+  end
+
+  def rewrite_json(memo_data)
     memo_to_hash = { memos: memo_data }
-    File.open(DB_PATH, "w") { |file| JSON.dump(memo_to_hash, file) }
+    File.open(DB_PATH, 'w') { |file| JSON.dump(memo_to_hash, file) }
+  end
+
+  def self.delete_memo(id)
+    DB_MEMOS.each do |memo|
+      DB_MEMOS.delete(memo) if memo[:id] == id.to_i
+    end
   end
 
   def self.match_id(id)
-    match_memo = ""
+    match_memo = ''
     DB_MEMOS.each { |memo| match_memo = memo if memo[:id] == id.to_i }
     match_memo
   end
 end
 
-get "/" do
+get '/' do
   @memos = DB_MEMOS
   erb :index
 end
 
-get "/new" do
+get '/new' do
   erb :new
 end
 
-get "/memos/:id" do
+get '/memos/:id' do
   @memo = Memo.match_id(params[:id])
-  erb :show
+  if @memo == ''
+    @error_message = 'This URL is not valid.'
+    erb :error
+  else
+    erb :show
+  end
 end
 
-get "/edit/:id" do
+get '/memos/*' do
+  'this id is not found..'
+end
+
+get '/edit/:id' do
   @memo = Memo.match_id(params[:id])
-  erb :edit
+  if @memo == ''
+    @error_message = 'This URL is not valid.'
+    erb :error
+  else
+    erb :edit
+  end
 end
 
-post "/new" do
-  memo = Memo.new(CGI.escape_html(params[:title]), CGI.escape_html(params[:body]))
-  Memo.rewrite_json(memo.add_memo)
-  redirect "/"
+post '/new' do
+  memo = Memo.new(params[:title], params[:body])
+  memo.rewrite_json(memo.add_memo)
+  redirect '/'
 end
 
-patch "/edit-done/:id" do
-  match_memo = Memo.match_id(params[:id])
-  change_memo =
-    DB_MEMOS.each do |memo|
-      if memo[:id] == match_memo[:id].to_i
-        memo[:title] = CGI.escape_html(params[:title])
-        memo[:body] = CGI.escape_html(params[:body])
-      end
-    end
-  Memo.rewrite_json(change_memo)
-  redirect "/"
-  erb :index
+patch '/edit-done/:id' do
+  memo = Memo.new(params[:title], params[:body])
+  memo.rewrite_json(memo.change_memo(params[:id]))
+  redirect '/'
 end
 
-delete "/delete/:id" do
-  delete_memo =
-    DB_MEMOS.each do |memo|
-      DB_MEMOS.delete(memo) if memo[:id] == params[:id].to_i
-    end
-  Memo.rewrite_json(delete_memo)
-  redirect "/"
-  erb :index
+delete '/delete/:id' do
+  Memo.delete_memo(params[:id])
+  redirect '/'
 end
 
 error 404 do
-  "404 not found sorry.."
+  '404 not found sorry..'
 end
