58ab623eb02c310bd80f0987ea44df805ba22712
ytakegawa <ytkgw1986@gmail.com>
2021-11-04 13:00:02 +0900

    Improve Methodize constants that reference the DB.
    DBを参照する定数をメソッド化した

--- app.rb
+++ app.rb
@@ -6,30 +6,33 @@ require 'json'
 require 'cgi'
 
 DB_PATH = 'json/memo_db.json'
-DB_DATA =
-  File.open(DB_PATH) do |file|
-    JSON.parse(file.read, symbolize_names: true)
-  end
 
 class Memo
   def initialize(title, body)
     @title = title
     @body = body
-    @db_memos = DB_DATA[:memos]
   end
 
   def add_memo
-    id = @db_memos.empty? ? 1 : @db_memos[-1][:id] + 1
-    @db_memos << { id: id, title: @title, body: @body }
+    id = Memo.load_memo.empty? ? 1 : Memo.load_memo[-1][:id] + 1
+    Memo.load_memo << { id: id, title: @title, body: @body }
   end
 
   def change_memo(id)
-    @db_memos.each do |memo|
+    Memo.load_memo.each do |memo|
       if memo[:id] == id.to_i
         memo[:title] = @title
         memo[:body] = @body
       end
     end
+  end
+
+  def self.load_memo
+    db_load =
+      File.open(DB_PATH) do |file|
+        JSON.parse(file.read, symbolize_names: true)
+      end
+    db_load[:memos]
   end
 
   def self.rewrite_json(memo_data)
@@ -38,19 +41,19 @@ class Memo
   end
 
   def self.delete_memo(id)
-    db_memos = DB_DATA[:memos]
-    db_memos.each do |memo|
-      db_memos.delete(memo) if memo[:id] == id.to_i
+    memos = Memo.load_memo
+    memos.each do |memo|
+      memos.delete(memo) if memo[:id] == id.to_i
     end
   end
 
   def self.match_id(id)
-    DB_DATA[:memos].find { |memo| memo[:id] == id.to_i }
+    Memo.load_memo.find { |memo| memo[:id] == id.to_i }
   end
 end
 
 get '/' do
-  @memos = DB_DATA[:memos]
+  @memos = Memo.load_memo
   erb :index
 end
 
